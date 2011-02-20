a88c9ad934399b85e7e31f6e1f9fefdd76d655a7
Dimitri Glazkov <dglazkov@chromium.org>
2011-02-20 14:25:33 -0800

    Added enough code to not barf on sample data.

---
+++ scripts/visualizer.js
@@ -0,0 +1,96 @@
+
+function layer()
+{
+    
+}
+
+layer.at = function(x, y)
+{
+    return chain;
+}
+
+
+function chain()
+{
+    return chain;
+}
+
+chain.at = layer.at;
+
+chain.size = function(w, h)
+{
+    return chain;
+}
+
+chain.contains = function()
+{
+    return chain;
+}
+
+chain.width = function()
+{
+    return chain;
+}
+
+chain.text = function()
+{
+    return chain;
+}
+
+chain.property = function()
+{
+    return chain;
+}
+
+function RenderView()
+{
+}
+
+RenderView.at = layer.at;
+
+function RenderBlock()
+{
+    return chain;
+}
+
+function RenderBody()
+{
+    return chain;
+}
+
+function RenderText()
+{
+    return chain;
+}
+
+function RenderVideo()
+{
+    return chain;
+}
+
+function textRun()
+{
+    
+}
+
+function RenderFlexibleBox()
+{
+    return chain;
+}
+
+function RenderButton()
+{
+    return chain;
+}
+
+function RenderSlider()
+{
+    return chain;
+}
+
+
+textRun.at = layer.at;
+
+var anonymous;
+var positioned;
+var relative = { positioned: '' }
