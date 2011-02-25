c2c3cd494a8d3ea264da2b2430f1bca963ca3990
Dimitri Glazkov <dglazkov@chromium.org>
2011-02-24 20:51:54 -0800

    Start rendering visualizations.

--- scripts/visualizer.js
+++ scripts/visualizer.js
@@ -1,29 +1,92 @@
-function chain()
+function Chain(type)
 {
-    return chain;
+    this.type_ = type;
+    this.createObject_();
+}
+
+var arena = document.createElement('div');
+
+Chain.prototype.createObject_ = function()
+{
+    this.div_ = arena.appendChild(document.createElement('div'));
+    this.div_.className = 'object';
+    this.div_.textContent = this.type_.name;
+}
+
+Chain.prototype.setParent = function(parent)
+{
+    parent.div_.appendChild(this.div_);
+}
+
+Chain.prototype.at = function(x, y)
+{
+    return this;
+}
+
+Chain.prototype.pos = function(positioning)
+{
+    return this;
+}
+
+Chain.prototype.tag = function(tag)
+{
+    return this;
+}
+
+Chain.prototype.size = function(w, h)
+{
+    return this;
+}
+
+Chain.prototype.width = function(w)
+{
+    return this;
+}
+
+Chain.prototype.property = function(name, value)
+{
+    return this;
+}
+
+Chain.prototype.text = function(t)
+{
+    return this;
+}
+
+Chain.prototype.contains = function()
+{
+    Array.prototype.slice.call(arguments, 0).forEach(function(child)
+    {
+        child.setParent(this)
+    }, this);
+    return this;
 }
 
 var anonymous;
 var positioned;
 var relative = { positioned: '' };
 
-[
-    'at',
-    'pos',
-    'tag',
-    'size',
-    'contains',
-    'width',
-    'property',
-    'text'
-].forEach(function(method) {
-    chain[method] = chain;
-});
+function createChainBuidler(type) {
+    return function(renderObject) {
+        return new Chain(type);
+    }
+}
 
 [
-    'layer',
-    'render',
-    'text',
+    {
+        name: 'layer',
+    },
+    {
+        name: 'render'
+    },
+    {
+        name: 'text'
+    }
 ].forEach(function(root) {
-    window[root] = chain;
+    window[root.name] = createChainBuidler(root);
 });
+
+window.addEventListener('DOMContentLoaded', function()
+{
+    document.body.appendChild(arena);
+}, false);
